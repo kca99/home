40bd38e8530ae1ee5f4597c6cc8a0335543c65a9
Kevin Chung <kevinchungw@gmail.com>
2021-10-28 01:33:52 -0700

    Playing with images and fonts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import background from "./assets/IMG_20191012_170346.jpg"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class = 'fullsceen'>
+      <div class = 'firstName'>
+      KEVIN
+      </div>
+      <div class = 'lastName'>
+      CHUNG
+      </div>
     </div>
   );
 }
